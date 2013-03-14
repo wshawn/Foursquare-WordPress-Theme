f81ce4d6af660c172f49c2a8fd42e88778e4512c
W. Shawn Wilkerson <shawn@shawnwilkerson.com>
2013-03-14 10:50:25 -0300

    Update 404.php
    It appears as if multiple things are being attempted on the same line:
    There is a malformed Ternary Operator
    There is an if presumably to check the existence of posts which hands off to a while which would perform the same check with the benefit of looping through the responses.
    
    Lastly, there is a call to the_post() which does not appear to be attached to anything, as it is a 404 response to begin with.

--- 404.php
+++ 404.php
@@ -11,7 +11,8 @@ get_header(); ?>
 
 <div class="row">
 	<section id="page" class="span7">	
-	<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
+// illegal code	<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
+	<?php (have_posts()) ? ( have_posts() ) : the_post(); ?> // untested correction
 		<h1><?php _e( 'Page Not Found', 'twentyten' ); ?></h1>
 			<p><?php _e( 'We\'re so sorry, but the page you are looking for has either moved or does not exist. Maybe searching for something else will help.', 'twentyten' ); ?></p>
 			<?php get_search_form(); ?>
@@ -23,3 +24,25 @@ get_header(); ?>
 <?php include ('sidebar-footer.php'); ?>
 
 <?php get_footer(); ?>
+
+/* Original twentyten template is below
+
+get_header(); ?>
+
+<div class="row">
+	<section id="page" class="span7">
+
+	<?php (have_posts()) ? ( have_posts() ) : the_post(); ?>
+		<h1><?php _e( 'Page Not Found', 'twentyten' ); ?></h1>
+			<p><?php _e( 'We\'re so sorry, but the page you are looking for has either moved or does not exist. Maybe searching for something else will help.', 'twentyten' ); ?></p>
+			<?php get_search_form(); ?>
+	</section><!--end page-->
+
+	<?php include ('sidebar-page.php'); ?>
+</div><!--end row-->
+
+<?php include ('sidebar-footer.php'); ?>
+
+<?php get_footer(); ?>
+
+*/
